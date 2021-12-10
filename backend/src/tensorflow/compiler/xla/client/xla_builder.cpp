4d5931821627daa9421efd5b4a05bff5a4140e16
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2021-12-10 18:53:50 +0000

    use references for arguments

--- backend/src/tensorflow/compiler/xla/client/xla_builder.cpp
+++ backend/src/tensorflow/compiler/xla/client/xla_builder.cpp
@@ -35,7 +35,6 @@ extern "C" {
     }
 
     c__XlaOp* c__XlaOp_operator_neg(c__XlaOp& s) {
-        // todo is this a memory leak?
         auto res = new XlaOp();
         *res = -reinterpret_cast<XlaOp&>(s);
         return reinterpret_cast<c__XlaOp*>(res);
@@ -81,18 +80,19 @@ extern "C" {
 
     struct c__XlaBuilder;
 
-    struct c__XlaBuilder* c__XlaBuilder_new() {
-        return reinterpret_cast<c__XlaBuilder*>(new XlaBuilder("XlaBuilder"));
+    c__XlaBuilder* c__XlaBuilder_new() {
+        auto builder = new XlaBuilder("XlaBuilder");
+        return reinterpret_cast<c__XlaBuilder*>(builder);
     }
 
     void c__XlaBuilder_del(c__XlaBuilder* s) {
         delete reinterpret_cast<XlaBuilder*>(s);
     }
 
-    const char* c__XlaBuilder_OpToString(c__XlaBuilder* s, c__XlaOp* op) {
-        auto s_ = reinterpret_cast<XlaBuilder*>(s);
-        auto op_ = reinterpret_cast<XlaOp*>(op);
-        auto op_str = s_->OpToString(*op_);
+    const char* c__XlaBuilder_OpToString(c__XlaBuilder& s, c__XlaOp& op) {
+        auto& s_ = reinterpret_cast<XlaBuilder&>(s);
+        auto& op_ = reinterpret_cast<XlaOp&>(op);
+        auto op_str = s_.OpToString(op_);
         char *res = NULL;
         res = (char *) malloc(op_str.length() + 1);
         strncpy(res, op_str.c_str(), op_str.length());
@@ -107,17 +107,18 @@ extern "C" {
      * 
      */
 
-    c__XlaOp* c__ConstantR0(c__XlaBuilder* builder, int32 value) {
+    c__XlaOp* c__ConstantR0(c__XlaBuilder& builder, int32 value) {
+        auto& builder_ = reinterpret_cast<XlaBuilder&>(builder);
         auto res = new XlaOp();
-        *res = ConstantR0(reinterpret_cast<XlaBuilder*>(builder), value);
+        *res = ConstantR0(&builder_, value);
         return reinterpret_cast<c__XlaOp*>(res);
     }
 
-    c__XlaOp* c__Eq(c__XlaOp* lhs, c__XlaOp* rhs) {
-        auto lhs_ = reinterpret_cast<XlaOp*>(lhs);
-        auto rhs_ = reinterpret_cast<XlaOp*>(rhs);
+    c__XlaOp* c__Eq(c__XlaOp& lhs, c__XlaOp& rhs) {
+        auto& lhs_ = reinterpret_cast<XlaOp&>(lhs);
+        auto& rhs_ = reinterpret_cast<XlaOp&>(rhs);
         auto res = new XlaOp();
-        *res = Eq(*lhs_, *rhs_);
+        *res = Eq(lhs_, rhs_);
         return reinterpret_cast<c__XlaOp*>(res);
     }
 
@@ -131,13 +132,13 @@ extern "C" {
      * 
      */
 
-    void* eval (c__XlaBuilder* builder, c__XlaOp* op) {
-        auto op_ = reinterpret_cast<XlaOp*>(op);
-        auto builder_ = reinterpret_cast<XlaBuilder*>(builder);
+    void* eval (c__XlaBuilder& builder, c__XlaOp& op) {
+        auto& op_ = reinterpret_cast<XlaOp&>(op);
+        auto& builder_ = reinterpret_cast<XlaBuilder&>(builder);
 
         ValueInferenceMode value_inf_mode;
-        Literal lit = (new ValueInference(builder_))
-            ->AnalyzeConstant(*op_, value_inf_mode)
+        Literal lit = (new ValueInference(&builder_))
+            ->AnalyzeConstant(op_, value_inf_mode)
             ->GetValue()
             ->Clone();  // todo we get memory errors if we don't do this. Not sure why.
 
@@ -150,7 +151,7 @@ extern "C" {
         return res;
     }
 
-    int32 eval_int32(c__XlaBuilder* builder, c__XlaOp* op) {
+    int32 eval_int32(c__XlaBuilder& builder, c__XlaOp& op) {
         return *(int32*) eval(builder, op);
     }
 }
