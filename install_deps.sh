2b65f05f8da065ce1a8c2574ecc26421e9c43bd9
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2022-03-27 18:50:39 +0100

    wip

--- install_deps.sh
+++ install_deps.sh
@@ -4,7 +4,8 @@ TMP_DIR=$(mktemp -d -p $(pwd))
 
 cd $TMP_DIR
 
-git clone --depth 1 https://github.com/Z-snails/Idris2-hashable.git
+git clone https://github.com/Z-snails/Idris2-hashable.git
+(cd Idris2-hashable; git checkout d97d2c39d9199941e2de1991224f564fc4b956dd)
 idris2 --install Idris2-hashable/hashable.ipkg
 
 cd - && rm -rf $TMP_DIR
