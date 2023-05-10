08cdc7b54ab272eb29fed266d87506c1849f6ffa
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-10 01:30:48 +0100

    wip

--- install_c_xla.sh
+++ install_c_xla.sh
@@ -5,7 +5,7 @@ mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 apt update && apt install -y bazel
 
-# Build C XLA interface
+# Build C XLA
 apt install -y wget
 wget -qO- https://github.com/elixir-nx/xla/releases/download/v0.3.0/xla_extension-x86_64-linux-$1.tar.gz | tar xvz -C backend/
 (cd backend; BAZEL_CXXOPTS='-std=c++14' bazel build //:c_xla_extension)
