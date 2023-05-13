6f595f8ed80f1b6de470b28687d01e1974b51d5b
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-13 21:40:05 +0100

    wip

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,3 @@
 curl -OL https://github.com/joelberkeley/spidr/releases/download/c-xla-$(cat backend/VERSION)/c_xla_extension-x86_64-linux-cpu.tar.gz
-sudo tar xzf c_xla_extension-x86_64-linux-cpu.tar.gz -C /usr/local/lib --strip-components 2
+sudo tar xzf c_xla_extension-x86_64-linux-cpu.tar.gz -C /usr/local/lib --strip-components 1
 sudo bash -c 'echo "/usr/local/lib/c_xla_extension/lib" >> /etc/ld.so.conf.d/c_xla_extension.conf' && sudo ldconfig
