ec6f6c16ee22d46857cae0948fc1cb4785ecdb7e
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-13 22:45:03 +0100

    install, uninstall XLA

--- preclean.sh
+++ preclean.sh
@@ -1,2 +1,7 @@
+rm -r /usr/local/lib/xla_extension
 rm -r /usr/local/lib/c_xla_extension
-rm /etc/ld.so.conf.d/c_xla_extension.conf && ldconfig
+
+rm /etc/ld.so.conf.d/xla_extension.conf
+rm /etc/ld.so.conf.d/c_xla_extension.conf
+
+ldconfig
--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,8 @@
+XLA_URL=https://github.com/elixir-nx/xla/releases/download/v0.3.0/xla_extension-x86_64-linux-cpu.tar.gz
 C_XLA_URL=https://github.com/joelberkeley/spidr/releases/download/c-xla-$(cat backend/VERSION)/c_xla_extension-x86_64-linux-cpu.tar.gz
+
+curl -s -L $XLA_URL | tar xz -C /usr/local/lib ./xla_extension/lib/libxla_extension.so
 curl -s -L $C_XLA_URL | tar xz -C /usr/local/lib --strip-components 1
+
+bash -c 'echo "/usr/local/lib/xla_extension/lib" >> /etc/ld.so.conf.d/xla_extension.conf' && ldconfig
 bash -c 'echo "/usr/local/lib/c_xla_extension/lib" >> /etc/ld.so.conf.d/c_xla_extension.conf' && ldconfig
