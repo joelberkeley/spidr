bd5cff79f473ea45163f29a5b097abefaabb858a
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-13 21:00:56 +0100

    whoops

---
+++ preclean.sh
@@ -0,0 +1,2 @@
+sudo rm -r /usr/local/lib/c_xla_extension
+sudo rm /etc/ld.so.conf.d/c_xla_extension.conf && sudo ldconfig
---
+++ preinstall.sh
@@ -0,0 +1,4 @@
+C_XLA_VERSION=$(cat VERSION)
+curl -OL https://github.com/joelberkeley/spidr/releases/download/c-xla-$C_XLA_VERSION/c_xla_extension-x86_64-linux-cpu.tar.gz
+sudo tar xzf c_xla_extension-x86_64-linux-cpu.tar.gz -C /usr/local/lib --strip-components 1
+sudo bash -c 'echo "/usr/local/lib/c_xla_extension/lib" >> /etc/ld.so.conf.d/c_xla_extension.conf' && sudo ldconfig
