54c2acc05ff2588b62279ac0bf26ed9a35a97564
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-13 22:53:41 +0100

    wip

--- preinstall.sh
+++ preinstall.sh
@@ -4,5 +4,7 @@ C_XLA_URL=https://github.com/joelberkeley/spidr/releases/download/c-xla-$(cat ba
 curl -s -L $XLA_URL | tar xz -C /usr/local/lib ./xla_extension/lib/libxla_extension.so
 curl -s -L $C_XLA_URL | tar xz -C /usr/local/lib --strip-components 1
 
-bash -c 'echo "/usr/local/lib/xla_extension/lib" >> /etc/ld.so.conf.d/xla_extension.conf' && ldconfig
-bash -c 'echo "/usr/local/lib/c_xla_extension/lib" >> /etc/ld.so.conf.d/c_xla_extension.conf' && ldconfig
+bash -c 'echo "/usr/local/lib/xla_extension/lib" >> /etc/ld.so.conf.d/xla_extension.conf'
+bash -c 'echo "/usr/local/lib/c_xla_extension/lib" >> /etc/ld.so.conf.d/c_xla_extension.conf'
+
+ldconfig
