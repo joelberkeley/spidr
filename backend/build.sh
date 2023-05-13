4adcd4ff3074c672c5cde0f8808254c3b40ae6f2
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-13 21:58:44 +0100

    wip

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,11 @@
 # Install Bazel
-sudo apt update && sudo apt upgrade -y && sudo apt install -y apt-transport-https curl gnupg
+apt update && apt upgrade -y && apt install -y apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
-sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-sudo apt update && sudo apt install -y bazel
+mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+apt update && apt install -y bazel
 
 # Build C XLA
-sudo apt install -y wget
+apt install -y wget
 wget -qO- https://github.com/elixir-nx/xla/releases/download/v0.3.0/xla_extension-x86_64-linux-$1.tar.gz | tar xz -C .
 BAZEL_CXXOPTS='-std=c++14' bazel build //:c_xla_extension
