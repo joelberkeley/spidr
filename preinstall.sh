320557e0c56340ef76f9d05ac5c70b87d5bae95f
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-13 22:16:20 +0100

    simplify scripts

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,3 @@
 C_XLA_URL=https://github.com/joelberkeley/spidr/releases/download/c-xla-$(cat backend/VERSION)/c_xla_extension-x86_64-linux-cpu.tar.gz
-curl -s -L $C_XLA_URL | tar xzf - -C /usr/local/lib --strip-components 1
+curl -s -L $C_XLA_URL | tar xz -C /usr/local/lib --strip-components 1
 bash -c 'echo "/usr/local/lib/c_xla_extension/lib" >> /etc/ld.so.conf.d/c_xla_extension.conf' && ldconfig
