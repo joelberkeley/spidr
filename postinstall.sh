7fe9fc2f35e6e985a4f3d5c659067e3964afb3c2
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-05-14 18:56:46 +0100

    abstract out common install instructions

--- postinstall.sh
+++ postinstall.sh
@@ -1,13 +1,20 @@
 XLA_EXT_VERSION=$(cat backend/XLA_EXT_VERSION)
 C_XLA_EXT_VERSION=$(cat backend/VERSION)
 
-echo "
-spidr Idris API installed. Now install either the CPU or CUDA backends. To install the CPU backend, run
+install_intructions() {
+  cat <<- EOF
+    curl -s -L https://github.com/elixir-nx/xla/releases/download/v$XLA_EXT_VERSION/xla_extension-x86_64-linux-$1.tar.gz | sudo tar xz -C /usr/local/lib ./xla_extension/lib/libxla_extension.so
+    curl -s -L https://github.com/joelberkeley/spidr/releases/download/c-xla-$C_XLA_EXT_VERSION/c_xla_extension-x86_64-linux-$1.tar.gz | sudo tar xz -C /usr/local/lib ./c_xla_extension/lib/libc_xla_extension.so
+    echo "/usr/local/lib/xla_extension/lib" | sudo tee -a /etc/ld.so.conf.d/xla_extension.conf
+    echo "/usr/local/lib/c_xla_extension/lib" | sudo tee -a /etc/ld.so.conf.d/c_xla_extension.conf
+    sudo ldconfig
+EOF
+}
 
-    curl -s -L https://github.com/elixir-nx/xla/releases/download/v$XLA_EXT_VERSION/xla_extension-x86_64-linux-cpu.tar.gz | sudo tar xz -C /usr/local/lib ./xla_extension/lib/libxla_extension.so
-    sudo bash -c 'echo \"/usr/local/lib/xla_extension/lib\" >> /etc/ld.so.conf.d/xla_extension.conf' && sudo ldconfig
-    curl -s -L https://github.com/joelberkeley/spidr/releases/download/c-xla-$C_XLA_EXT_VERSION/c_xla_extension-x86_64-linux-cpu.tar.gz | sudo tar xz -C /usr/local/lib ./c_xla_extension/lib/libc_xla_extension.so
-    sudo bash -c 'echo \"/usr/local/lib/c_xla_extension/lib\" >> /etc/ld.so.conf.d/c_xla_extension.conf' && sudo ldconfig
+echo "
+spidr Idris API installed. Now install either the CPU or CUDA backend. To install the CPU backend, run
+
+$(install_intructions cpu)
 
 Or, to install the CUDA backend, install the NVIDIA prerequisites for running TensorFlow on GPU, as listed on the TensorFlow GPU installation page (there is no need to install TensorFlow itself)
 
@@ -15,10 +22,7 @@ Or, to install the CUDA backend, install the NVIDIA prerequisites for running Te
 
 then run
 
-    curl -s -L https://github.com/elixir-nx/xla/releases/download/v$XLA_EXT_VERSION/xla_extension-x86_64-linux-cuda111.tar.gz | sudo tar xz -C /usr/local/lib ./xla_extension/lib/libxla_extension.so
-    sudo bash -c 'echo \"/usr/local/lib/xla_extension/lib\" >> /etc/ld.so.conf.d/xla_extension.conf' && sudo ldconfig
-    curl -s -L https://github.com/joelberkeley/spidr/releases/download/c-xla-$C_XLA_EXT_VERSION/c_xla_extension-x86_64-linux-cuda111.tar.gz | sudo tar xz -C /usr/local/lib ./c_xla_extension/lib/libc_xla_extension.so
-    sudo bash -c 'echo \"/usr/local/lib/c_xla_extension/lib\" >> /etc/ld.so.conf.d/c_xla_extension.conf' && sudo ldconfig
+$(install_intructions cuda111)
 
 When you uninstall spidr, you may wish to remove installed XLA artifacts
 
