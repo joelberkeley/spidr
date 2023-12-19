e010b28c9604cb8646165dd1a2168757f54f980b
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2023-12-19 00:20:01 +0000

    pin Bazel version; remove unused Idris dependencies (#377)

--- backend/install_bazel.sh
+++ backend/install_bazel.sh
@@ -2,4 +2,4 @@ apt update && apt upgrade -y && apt install -y apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt update && apt install -y bazel
+apt update && apt install -y bazel-6.4.0
