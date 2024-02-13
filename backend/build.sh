e4d5bcae7cc03f1c191a46724bc268dc6be94b55
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-13 20:02:43 +0000

    add CI for macos

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,2 @@
-curl -s -L https://github.com/elixir-nx/xla/releases/download/v$(cat XLA_EXT_VERSION)/xla_extension-x86_64-linux-$1.tar.gz | tar xz
+curl -s -L https://github.com/elixir-nx/xla/releases/download/v$(cat XLA_EXT_VERSION)/xla_extension-$1.tar.gz | tar xz
 BAZEL_CXXOPTS='-std=c++14' bazel-6.4.0 build //:c_xla_extension
