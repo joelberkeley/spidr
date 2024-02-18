da8da8b51faaf490e20d16ede09d386b6c1ff05e
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-18 14:15:57 +0000

    wip

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,5 @@
+set -e
+set -o pipefail
+
 curl -fL https://github.com/elixir-nx/xla/releases/download/v$(cat XLA_EXT_VERSION)/xla_extension-$1.tar.gz | tar xz
 BAZEL_CXXOPTS='-std=c++14' ./bazelisk build //:c_xla_extension
