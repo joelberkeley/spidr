9a5ba0dded05fc689bb0dd07c8015cbf14ba76a6
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-18 14:52:29 +0000

    wip

--- backend/install_bazel.sh
+++ backend/install_bazel.sh
@@ -2,5 +2,5 @@ set -e
 set -o pipefail
 
 apt update && apt install -y curl build-essential
-curl -fL https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-arm64 > bazelisk
+curl -fL https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 > bazelisk
 chmod +x bazelisk
