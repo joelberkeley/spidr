5b40caec21e3c458bfec1c40b4d1680242a6117d
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-18 13:33:05 +0000

    wip

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,2 @@
 curl -s -L https://github.com/elixir-nx/xla/releases/download/v$(cat XLA_EXT_VERSION)/xla_extension-$1.tar.gz | tar xz
-BAZEL_CXXOPTS='-std=c++20' bazel build //:c_xla_extension
+BAZEL_CXXOPTS='-std=c++14' bazel build //:c_xla_extension
