f3b36c62b0611628da406d851322b3c07189b81d
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-18 00:49:53 +0000

    wip

--- backend/install_bazel.sh
+++ backend/install_bazel.sh
@@ -1,5 +1,4 @@
-apt update && apt upgrade -y && apt install -y apt-transport-https curl gnupg
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
-mv bazel.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt update && apt install -y bazel-6.1.2
+apt update && apt install -y build-essential
+curl -fL https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-arm64 > bazelisk
+chmod +x bazelisk
+./bazelisk
