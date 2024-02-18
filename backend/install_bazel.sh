d7819e048d47d56797f029e08a0075463a5a74f7
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-18 14:15:57 +0000

    wip

--- backend/install_bazel.sh
+++ backend/install_bazel.sh
@@ -1,3 +1,6 @@
+set -e
+set -o pipefail
+
 apt update && apt install -y curl build-essential
 curl -fL https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-arm64 > bazelisk
 chmod +x bazelisk
--- backend/package.sh
+++ backend/package.sh
@@ -1,3 +1,6 @@
+set -e
+set -o pipefail
+
 DIR=$(mktemp -d)
 mkdir -p $DIR/c_xla_extension/lib
 mv bazel-bin/libc_xla_extension.so $DIR/c_xla_extension/lib
