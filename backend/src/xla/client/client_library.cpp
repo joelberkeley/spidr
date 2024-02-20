19f9b19a3bbdfbf38df36915c2c634c2bb21809e
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-20 22:54:12 +0000

    wip

--- backend/src/xla/client/client_library.cpp
+++ backend/src/xla/client/client_library.cpp
@@ -34,7 +34,7 @@ extern "C" {
 //        *xla::ClientLibrary::GetOrCreateLocalClient();
         std::cout << "... worked with cpu " << std::endl;
 
-        const auto& client = *xla::ClientLibrary::GetOrCreateLocalClient(platform_, std::nullopt);
+        auto* client = *xla::ClientLibrary::GetOrCreateLocalClient(platform_, std::nullopt);
 
         return reinterpret_cast<LocalClient*>(client);
     }
