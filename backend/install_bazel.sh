0ab8b4a10fcae030f17e34f0e27ef73aa8792fca
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-02-23 20:24:01 +0000

    build XLA myself

--- backend/install_bazel.sh
+++
@@ -1,6 +0,0 @@
-set -e
-set -o pipefail
-
-apt update && apt install -y curl build-essential
-curl -fL https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 > bazelisk
-chmod +x bazelisk
