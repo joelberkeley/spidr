5e4d151ec5edbbcca819671914eb2bb22327d58f
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-03-17 01:50:30 +0000

    try cache

--- backend/src/xla/pjrt/c/pjrt_c_api_cpu.cpp
+++ backend/src/xla/pjrt/c/pjrt_c_api_cpu.cpp
@@ -13,10 +13,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#include "xla/pjrt/c/pjrt_c_api_cpu.h"
+
 #include "pjrt_c_api_cpu.h"
 
 extern "C" {
-//  const PJRT_Api* GetPjrtApi() {
-//    return GetPjrtApi();
-//  }
+  const PJRT_Api* GetPjrtApi() {
+    return GetPjrtApi();
+  }
 }
