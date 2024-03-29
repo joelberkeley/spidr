5153c36c896f529e1a30921568692c8ccd4370d9
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-03-29 13:52:04 +0000

    fix xla install

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@ mkdir xla
 cd xla || exit
 git init
 git remote add origin https://github.com/openxla/xla
-git fetch origin "$(cat "$XLA_REVISION")"
+git fetch origin "$(cat ../XLA_REVISION)"
 git reset --hard FETCH_HEAD
 ./configure.py --backend=CPU
 cd - || exit
