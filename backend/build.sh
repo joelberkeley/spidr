17a5ef3de78373b3e16859f3fdee6763c2892a53
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-03-29 18:13:51 +0000

    cc_binary

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,2 @@
-mkdir xla
-cd xla || exit
-git init
-git remote add origin https://github.com/openxla/xla
-git fetch origin "$(cat ../XLA_REVISION)"
-git reset --hard FETCH_HEAD
-./configure.py --backend=CPU
-cd - || exit
-
 # maybe --logging [4, 5 or 6]
-BAZEL_CXXOPTS=-std=c++17 bazel build --verbose_failures --experimental_repo_remote_exec //:c_xla
+BAZEL_CXXOPTS=-std=c++17 bazel build --verbose_failures --experimental_cc_shared_library --experimental_repo_remote_exec //:c_xla
