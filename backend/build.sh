fc557fdaf4c2bb1a555ee2f15067d1883f327f66
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-03-29 18:14:38 +0000

    wip

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,2 @@
 # maybe --logging [4, 5 or 6]
-BAZEL_CXXOPTS=-std=c++17 bazel build --verbose_failures --experimental_cc_shared_library --experimental_repo_remote_exec //:c_xla
+BAZEL_CXXOPTS=-std=c++17 bazel build --verbose_failures --experimental_repo_remote_exec //:c_xla
