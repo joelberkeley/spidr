cc95403f6b7566a40d85c2d27dee3e9a4c2a511f
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-04-20 17:43:51 +0100

    cleanup

--- backend/cleanup.sh
+++ backend/cleanup.sh
@@ -1,2 +1,4 @@
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 20
-sudo docker image ls
+sudo dpkg --remove libcudnn8 libcudnn8-dev libnvinfer8 libnvinfer-dev
+sudo apt-get autoremove
+sudo apt-get autoclean
