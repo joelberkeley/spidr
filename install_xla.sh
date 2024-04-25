0001867b7312b3c3530f4d840487b332203c9231
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-04-25 21:45:49 +0100

    workflow fixes

--- install_xla.sh
+++ install_xla.sh
@@ -3,10 +3,7 @@ if [ -d xla ]; then
   exit 1;
 fi
 
-mkdir xla
-cd xla || exit
 git init
 git remote add origin https://github.com/openxla/xla
 git fetch origin "$(cat ../XLA_COMMIT_SHA)"
 git reset --hard FETCH_HEAD
-cd - || exit
