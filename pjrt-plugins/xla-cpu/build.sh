0b503b5deb5713f67dc785d8bb2509a9f6bec617
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-05-12 01:53:06 +0100

    wip

--- pjrt-plugins/xla-cpu/build.sh
+++ pjrt-plugins/xla-cpu/build.sh
@@ -1,8 +1,12 @@
 set -e
 
-../../install_xla.sh
-cd xla
-./configure.py --backend=CPU
-bazel build //xla/pjrt/c:pjrt_c_api_cpu_plugin.so
-mv bazel-bin/xla/pjrt/c/pjrt_c_api_cpu_plugin.so ../pjrt_plugin_xla_cpu.so
-cd ..
+source dev.sh
+rev=$(cat XLA_VERSION)
+xla_dir=$(install_xla $rev)
+(
+  cd xla_dir
+  ./configure.py --backend=CPU
+  bazel build //xla/pjrt/c:pjrt_c_api_cpu_plugin.so
+)
+mv xla_dir/bazel-bin/xla/pjrt/c/pjrt_c_api_cpu_plugin.so pjrt_plugin_xla_cpu.so
+rm -rf xla_dir
