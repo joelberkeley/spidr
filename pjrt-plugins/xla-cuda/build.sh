079054f2bdaef92a9efa8875ecb93e2f195aa7c2
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-05-15 01:00:14 +0100

    reinstate CUDA builds

--- pjrt-plugins/xla-cuda/build.sh
+++ pjrt-plugins/xla-cuda/build.sh
@@ -1,8 +1,12 @@
 set -e
 
-../../install_xla.sh
-cd xla
-# note we're not using ./configure.py, see https://github.com/openxla/xla/issues/12017
-bazel build --nobuild_runfile_links --nolegacy_external_runfiles --config release_gpu_linux //xla/pjrt/c:pjrt_c_api_gpu_plugin.so
-mv bazel-bin/xla/pjrt/c/pjrt_c_api_gpu_plugin.so ../pjrt_plugin_xla_cuda.so
-cd ..
+source dev.sh
+xla_dir=$(mktemp -d)
+install_xla $xla_dir
+(
+  cd $xla_dir
+  # note we're not using ./configure.py, see https://github.com/openxla/xla/issues/12017
+  bazel build --config release_gpu_linux //xla/pjrt/c:pjrt_c_api_gpu_plugin.so
+)
+mv $xla_dir/bazel-bin/xla/pjrt/c/pjrt_c_api_cpu_plugin.so pjrt_plugin_xla_cpu.so
+rm -rf $xla_dir
