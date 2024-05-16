7bde10df07c2551878c9cf2ed3f93201c996b5d7
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-05-16 17:59:05 +0100

    disable cuda build

--- pjrt-plugins/xla-cuda/build.sh
+++ pjrt-plugins/xla-cuda/build.sh
@@ -6,7 +6,8 @@ xla_dir=$(mktemp -d)
 install_xla $xla_dir
 (
   cd $xla_dir
-  # note we're not using ./configure.py, see https://github.com/openxla/xla/issues/12017
+  # note we're not using `./configure.py --backend=CUDA` as it requires a GPU, but the build
+  # itself doesn't, see https://github.com/openxla/xla/issues/12017
   bazel build --config release_gpu_linux //xla/pjrt/c:pjrt_c_api_gpu_plugin.so
 )
 mv $xla_dir/bazel-bin/xla/pjrt/c/pjrt_c_api_gpu_plugin.so pjrt_plugin_xla_cuda.so
