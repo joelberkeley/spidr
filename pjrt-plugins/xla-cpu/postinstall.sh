da5ecfa5673c3ad8521a9fcde90a0b910c59807c
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-05-24 02:27:37 +0100

    wip

--- pjrt-plugins/xla-cpu/postinstall.sh
+++ pjrt-plugins/xla-cpu/postinstall.sh
@@ -6,14 +6,14 @@ cd "$script_dir/../.."
 rev=$(cat XLA_VERSION)
 cd - > /dev/null 2>&1
 
-bin_uri="https://github.com/joelberkeley/spidr/releases/download/xla-$(short_revision "$rev")/pjrt_plugin_xla_cpu.so"
+lib_uri="https://github.com/joelberkeley/spidr/releases/download/xla-$(short_revision "$rev")/pjrt_plugin_xla_cpu.so"
 
 if [ "$SPIDR_MANUAL_INSTALL" = 0 ] || [ -z "$SPIDR_MANUAL_INSTALL" ]; then
-  curl -sLO "$bin_uri" --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cpu-0.0.1/lib"
+  curl -sLO "$lib_uri" --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cpu-0.0.1/lib"
 elif [ "$SPIDR_MANUAL_INSTALL" = 1 ]; then
   echo "Idris API installed. Now install supporting libraries to your library path."
   echo ""
-  echo "    $bin_uri"
+  echo "    $lib_uri"
   echo ""
 else
   echo "Invalid value $SPIDR_MANUAL_INSTALL found for SPIDR_MANUAL_INSTALL. Expected 0 or 1 if set."
--- spidr/postinstall.sh
+++ spidr/postinstall.sh
@@ -3,14 +3,14 @@
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 c_xla_version=$(cat "$script_dir/backend/VERSION")
 
-bin_uri="https://github.com/joelberkeley/spidr/releases/download/c-xla-v$c_xla_version/libc_xla.so"
+lib_uri="https://github.com/joelberkeley/spidr/releases/download/c-xla-v$c_xla_version/libc_xla.so"
 
 if [ "$SPIDR_MANUAL_INSTALL" = 0 ] || [ -z "$SPIDR_MANUAL_INSTALL" ]; then
-  curl -sLO "$bin_uri" --create-dirs --output-dir "$(idris2 --libdir)/spidr-0.0.6/lib"
+  curl -sLO "$lib_uri" --create-dirs --output-dir "$(idris2 --libdir)/spidr-0.0.6/lib"
 elif [ "$SPIDR_MANUAL_INSTALL" = 1 ]; then
   echo "Idris API installed. Now install supporting libraries to your library path."
   echo ""
-  echo "    $bin_uri"
+  echo "    $lib_uri"
   echo ""
 else
   echo "Invalid value $SPIDR_MANUAL_INSTALL found for SPIDR_MANUAL_INSTALL. Expected 0 or 1 if set."
