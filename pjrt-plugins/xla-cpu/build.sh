4910b4e2a0a11d002d695a379304a98c0e44846b
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-11-19 21:27:28 +0000

    wip

--- pjrt-plugins/xla-cpu/build.sh
+++ pjrt-plugins/xla-cpu/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -e
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  os="linux"
+  bin_ext=".so"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  os="darwin"
+  bin_ext=".dylib"
+else
+  echo "OS ${OSTYPE} not handled, expected linux-gnu or darwin"
+  exit 1
+fi
+
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 cd "$script_dir/../.."
 . ./dev.sh
@@ -9,7 +20,7 @@ xla_dir=$(mktemp -d)
 install_xla "$rev" "$xla_dir"
 (
   cd "$xla_dir"
-  ./configure.py --backend=CPU --os=$1
+  ./configure.py --backend=CPU --os=$os
   bazel build //xla/pjrt/c:pjrt_c_api_cpu_plugin.so
 )
-mv "$xla_dir/bazel-bin/xla/pjrt/c/pjrt_c_api_cpu_plugin.so" pjrt_plugin_xla_cpu-$1.so
+mv "$xla_dir/bazel-bin/xla/pjrt/c/pjrt_c_api_cpu_plugin.so" "pjrt_plugin_xla_cpu-${os}${bin_ext}"
--- spidr/backend/build.sh
+++ spidr/backend/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -e
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  os="linux"
+  bin_ext=".so"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  os="darwin"
+  bin_ext=".dylib"
+else
+  echo "OS ${OSTYPE} not handled, expected linux-gnu or darwin"
+  exit 1
+fi
+
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 cd "$script_dir/../.."
 . ./dev.sh
@@ -9,7 +20,8 @@ rev="$(cat XLA_VERSION)"
   cd spidr/backend
   mkdir xla
   install_xla "$rev" xla
+  (cd xla; ./configure.py --backend=cpu --os=$os)
   bazel build //:c_xla
   rm -rf xla
 )
-#mv spidr/backend/bazel-bin/libc_xla.so libc_xla-linux.so
+mv "spidr/backend/bazel-bin/libc_xla${bin_ext}" "libc_xla-${os}${bin_ext}"
