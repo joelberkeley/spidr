e07c7f8c72e0a32829954f82a896f037331f6fdf
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-12-02 22:48:21 +0000

    wip

--- spidr/backend/src/xla/hlo/translate/portable_api.cpp
+++ spidr/backend/src/xla/hlo/translate/portable_api.cpp
@@ -22,6 +22,8 @@ limitations under the License.
 extern "C" {
     string* ConvertHloToStablehlo(HloModule& hlo_module) {
         auto& hlo_module_ = reinterpret_cast<xla::HloModule&>(hlo_module);
+        // the implementation of this function shows how to get the actual MLIR module, which is
+        // crucial for enzyme!
         auto res = xla::ConvertHloToStablehlo(hlo_module_, true);
         return reinterpret_cast<string*>(new std::string(res.value()));
     }
