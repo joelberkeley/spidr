de00a64b2603e6d34b247c768119051e67a2a3f3
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2024-12-08 01:14:40 +0000

    wip

--- spidr/backend/src/stablehlo/dialect/Serialization.cpp
+++ spidr/backend/src/stablehlo/dialect/Serialization.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "mlir/Bytecode/BytecodeWriter.h"
 #include "stablehlo/dialect/Serialization.h"
 #include "stablehlo/dialect/Version.h"
 
@@ -24,21 +23,7 @@ extern "C" {
     int serializePortableArtifact(ModuleOp& module, string& str) {
         auto& module_ = reinterpret_cast<mlir::ModuleOp&>(module);
         auto& str_ = reinterpret_cast<std::string&>(str);
-
-//        std::string s;
-//        llvm::raw_string_ostream os0(s);
-//        module_.print(os0);
-//        printf("serializePortableArtifact ...\n");
-//        printf("... debug print:\n");
-//        printf("%s\n", s.c_str());
-
         llvm::raw_string_ostream os(str_);
-//        if (mlir::writeBytecodeToFile(module_, os).failed()) {
-//            return (int) false;
-//        }
-
-//        printf("... serialization:\n");
-//        printf("%s\n", str_.c_str());
         auto version = mlir::vhlo::Version::getMinimumVersion().toString();
         auto result = mlir::stablehlo::serializePortableArtifact(module_, version, os);
         return (int) result.succeeded();
@@ -49,9 +34,6 @@ extern "C" {
         auto str = new std::string();
         llvm::raw_string_ostream os(*str);
         module_.print(os);
-
-        printf("... debug print:\n");
-        printf("%s\n", str->c_str());
         return reinterpret_cast<string*>(str);
     }
 }
