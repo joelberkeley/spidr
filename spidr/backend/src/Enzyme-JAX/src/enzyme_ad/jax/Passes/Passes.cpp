0fd9a32fd37a8b5a7315621fd1f04b997472eaf4
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2025-03-24 23:14:50 +0000

    year

--- spidr/backend/src/Enzyme-JAX/src/enzyme_ad/jax/Passes/Passes.cpp
+++ spidr/backend/src/Enzyme-JAX/src/enzyme_ad/jax/Passes/Passes.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Joel Berkeley
+Copyright 2025 Joel Berkeley
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
