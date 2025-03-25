6257e348d01a02060582f9369253774042c7b068
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2025-03-25 23:28:54 +0000

    wip

--- spidr/backend/build.sh
+++ spidr/backend/build.sh
@@ -26,15 +26,15 @@ esac
 
 (
   cd spidr/backend
-#  mkdir xla
-#  install_xla "$xla_rev" xla
+  mkdir xla
+  install_xla "$xla_rev" xla
   (cd xla; ./configure.py --backend=cpu --os=$os)
   # depending on Enzyme-JAX is problematic as it fixes the XLA version. Can we only depend on enzyme?
   # seems unlikely that they could decouple XLA entirely. They almost certainly can't decouple stablehlo
-#  mkdir Enzyme-JAX
-#  install_enzyme "$enzyme_rev" Enzyme-JAX
-#  patch Enzyme-JAX/src/enzyme_ad/jax/BUILD < BUILD.patch
+  mkdir Enzyme-JAX
+  install_enzyme "$enzyme_rev" Enzyme-JAX
+  patch Enzyme-JAX/src/enzyme_ad/jax/BUILD < BUILD.patch
   bazel build //:c_xla
-#  rm -rf xla Enzyme-JAX
+  rm -rf xla Enzyme-JAX
 )
 mv "spidr/backend/bazel-bin/libc_xla.$ext" "libc_xla-$os-$arch.$ext"
