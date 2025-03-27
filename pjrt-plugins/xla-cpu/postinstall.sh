d79b17b2e93d61b79bbb606d3af489d9a4d273de
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2025-03-27 01:12:59 +0000

    wip

--- pjrt-plugins/xla-cpu/postinstall.sh
+++ pjrt-plugins/xla-cpu/postinstall.sh
@@ -4,7 +4,8 @@ if [ "$SPIDR_INSTALL_SUPPORT_LIBS" = false ]; then exit 0; fi
 
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 cd "$script_dir"
-rev=$(cat XLA_VERSION)
+rev=$(cat VERSION)
+cd - > /dev/null 2>&1
 
 os="$(uname)"
 case $os in
@@ -23,4 +24,4 @@ case $os in
 esac
 
 curl -fsL "https://github.com/joelberkeley/spidr/releases/download/pjrt-plugin-xla-cpu-$platform-v$rev/pjrt_plugin_xla_cpu.$ext" \
-  -o "pjrt_plugin_xla_cpu.$ext" --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cpu-v$rev/lib"
+  -o "pjrt_plugin_xla_cpu.$ext" --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cpu-$rev/lib"
--- pjrt-plugins/xla-cuda/postinstall.sh
+++ pjrt-plugins/xla-cuda/postinstall.sh
@@ -3,9 +3,8 @@
 if [ "$SPIDR_INSTALL_SUPPORT_LIBS" = false ]; then exit 0; fi
 
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
-cd "$script_dir/../.."
-. ./dev.sh
-rev=$(cat XLA_VERSION)
+cd "$script_dir"
+rev=$(cat VERSION)
 cd - > /dev/null 2>&1
 
 os="$(uname)"
@@ -21,4 +20,4 @@ esac
 # check the versions are the same as in .ipkg files - a simple grep for `version=$rev`
 # should also delete previous versions?
 curl -fsL "https://github.com/joelberkeley/spidr/releases/download/pjrt-plugin-xla-cuda-linux-x86_64-v$rev/pjrt_plugin_xla_cuda.so" \
-  -o pjrt_plugin_xla_cuda.so --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cuda-v$rev/lib"
+  -o pjrt_plugin_xla_cuda.so --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cuda-$rev/lib"
