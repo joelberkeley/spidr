abcde04f19d8d0aeff9477de00f004ca25d614ba
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2025-04-26 03:18:06 +0000

    local plugin versions (#456)

--- pjrt-plugins/xla-cuda/postinstall.sh
+++ pjrt-plugins/xla-cuda/postinstall.sh
@@ -3,9 +3,8 @@
 if [ "$SPIDR_INSTALL_SUPPORT_LIBS" = false ]; then exit 0; fi
 
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
-cd "$script_dir/../.."
-. ./dev.sh
-rev=$(cat XLA_VERSION)
+cd "$script_dir"
+rev=$(cat VERSION)
 cd - > /dev/null 2>&1
 
 os="$(uname)"
@@ -18,5 +17,5 @@ case $os in
     ;;
 esac
 
-curl -fsL "https://github.com/joelberkeley/spidr/releases/download/xla-$(short_revision "$rev")/pjrt_plugin_xla_cuda-linux-x86_64.so" \
-  -o pjrt_plugin_xla_cuda.so --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cuda-0.0.1/lib"
+curl -fsL "https://github.com/joelberkeley/spidr/releases/download/pjrt-plugin-xla-cuda-v$rev/pjrt_plugin_xla_cuda-linux-x86_64.so" \
+  -o pjrt_plugin_xla_cuda.so --create-dirs --output-dir "$(idris2 --libdir)/pjrt-plugin-xla-cuda-$rev/lib"
